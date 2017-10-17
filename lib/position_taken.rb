66899784fe83a71eb5058585eb3a92111eb66b64
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-17 15:23:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
   return true
-  elsif board[index] == " " || board[index] == " " || board[index] == nil
-    return false
-  elsif board[index] == " "
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
     return false
   end
 end
